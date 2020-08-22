c9ed7377fc9cd146605dc318caa5bd50b7bc3d21
Kaiser Shaik <kaiser.shaik@gmail.com>
2020-08-22 16:21:24 +0530

    Set up imports and working env

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+# ML Libraries
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+# Global Parameters
+stop_words = set(stopwords.words('english'))

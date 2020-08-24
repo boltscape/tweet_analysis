b32cc9129fafb808482e23394e20710ced24fef8
Kaiser Shaik <kaiser.shaik@gmail.com>
2020-08-24 10:06:15 +0530

    Added trending tweet and hashtag fetching

--- bot.py
+++ bot.py
@@ -46,3 +46,29 @@ def get_woeid(api, locations):
             print("err: ",location," woeid does not exist in trending topics")
     return woeids
 
+# Fetching popular tweets from API
+def get_tweets(api, query):
+    tweets = []
+    for status in tweepy.Cursor(api.search, q=query, count=1000, result_type='popular', include_entities=True, monitor_rate_limit=True,  wait_on_rate_limit=True, lang="en").items():  
+        # Fetching only English tweets
+        if isEnglish(status.text):
+            tweets.append([status.id_str, query, status.created_at.strftime('%d-%m-%Y %H:%M'), status.user.screen_name, status.text])
+    return tweets
+
+# Fetch trending hashtags
+def get_trending_hashtags(api, location):
+    woeids = get_woeid(api, location)
+    trending_tags = set()
+    for woeid in woeids:
+        try:
+            trends = api.trends_place(woeid)
+        except:
+            print("API limit exceeded. Trying again in 1 hour.")
+            time.sleep(3605)
+            trends = api.trends_place(woeid)
+        # Filtering out only English tweets and hash symbols
+        topics = [trend['name'][1:] for trend in trends[0]['trends'] if (trend['name'].find('#') == 0 and isEnglish(trend['name']) == True)]
+        trending_tags.update(topics)
+    
+    return trending_tags
+
